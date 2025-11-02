bc4614c7d0b012fa930c1564a6c1411d2ab1ae58
Vova <vovakucin082@gmail.com>
2025-11-02 18:48:26 +0100

    Corrected

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 class Distance:
 
-    def __init__(self, km: int | float) -> None :
+    def __init__(self, km: int | float) -> None:
         self.km = km
 
     def __str__(self) -> str:
